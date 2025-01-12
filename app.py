8370c431529e1507e991fdd80bc41d2ad3fc55c8
Alen Subasic <alen.subasic@dhigroupinc.com>
2025-01-12 11:05:57 -0600

    Update video download format to prioritize best video quality

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def send_webhook_notification(title, message, link):
 def download_video(video_id, video_title):
     ydl_opts = {
         "outtmpl": "/usr/src/app/downloads/%(title)s.%(ext)s",  # Update the path here
-        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+        "format": "bestvideo[ext=mp4]",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
